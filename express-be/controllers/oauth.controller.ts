c31474c14966e2ed39a55f08fbd52655e7ebf3cf
Madusanka Dissanayake <mdissanayake994@gmail.com>
2025-12-23 12:14:46 +0530

    refactor(global error handler) : refactor oauth controller and service to use global error handler

--- express-be/controllers/oauth.controller.ts
+++ express-be/controllers/oauth.controller.ts
@@ -9,75 +9,60 @@ export class OAuthController {
     private oAuthservice = new OAuthService();
 
     login = (req: Request, res: Response, next: NextFunction) => {
-        console.log('login hit!');
-        try {
-            const state = crypto.randomBytes(32).toString('hex');
-            req.session.state = state;
-            const authorizationUrl = this.oAuthservice.generateAuthUrl(state);
-            res.redirect(authorizationUrl);
 
-        } catch (error) {
-            console.error('Login error:', error);
-            next(error);
-        }
+        const state = crypto.randomBytes(32).toString('hex');
+        req.session.state = state;
+        const authorizationUrl = this.oAuthservice.generateAuthUrl(state);
+        res.redirect(authorizationUrl);
+
     }
 
     callback = async (req: Request<unknown, unknown, unknown, GoogleCallbackRequestQueryDTO>, res: Response, next: NextFunction) => {
-        console.log('call back hit!');
-        try {
 
-            const { code, error, state } = req.query;
+        const { code, error, state } = req.query;
 
-            if (error) {
-                console.log('Error from google:', error)
-                return res.redirect(`${Environment.FRONTEND_URL}/?auth=failed&error=${encodeURIComponent(error)}`);
-            }
+        if (error) {
+            console.log('Error from google:', error)
+            return res.redirect(`${Environment.FRONTEND_URL}/?auth=failed&error=${encodeURIComponent(error)}`);
+        }
 
-            if (state !== req.session?.state) {
-                console.error('State mismatch. Possible CSRF attack');
-                return res.redirect(`${Environment.FRONTEND_URL}/?auth=failed&error=csrf`);
-            }
+        if (state !== req.session?.state) {
+            console.error('State mismatch. Possible CSRF attack');
+            return res.redirect(`${Environment.FRONTEND_URL}/?auth=failed&error=csrf`);
+        }
 
-            const { tokens, user: googleUser } = await this.oAuthservice.getGoogleUser(code);
+        const { tokens, user: googleUser } = await this.oAuthservice.getGoogleUser(code);
 
-            if (googleUser.id && googleUser.email && googleUser.name && googleUser.picture) {
-                const user = await this.oAuthservice.createOrUpdateUser({
-                    id: googleUser.id,
-                    email: googleUser.email,
-                    name: googleUser.name,
-                    picture: googleUser.picture
-                });
+        if (googleUser.id && googleUser.email && googleUser.name && googleUser.picture) {
+            const user = await this.oAuthservice.createOrUpdateUser({
+                id: googleUser.id,
+                email: googleUser.email,
+                name: googleUser.name,
+                picture: googleUser.picture
+            });
 
-                // Generate new Access Token
-                const refreshToken = await this.oAuthservice.generateRefreshToken(user);
+            // Generate new Access Token
+            const refreshToken = await this.oAuthservice.generateRefreshToken(user);
 
-                // Save refresh token to DB
-                await this.oAuthservice.saveRefreshToken(user.id, refreshToken);
+            // Save refresh token to DB
+            await this.oAuthservice.saveRefreshToken(user.id, refreshToken);
 
-                res.cookie('refreshToken', refreshToken, {
-                    httpOnly: true,
-                    secure: Environment.NODE_ENV === 'production',
-                    sameSite: 'lax',
-                    maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
-                });
+            res.cookie('refreshToken', refreshToken, {
+                httpOnly: true,
+                secure: Environment.NODE_ENV === 'production',
+                sameSite: 'lax',
+                maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+            });
 
-                req.session.credentials = tokens;
-                req.session.userId = user.id;
+            req.session.credentials = tokens;
+            req.session.userId = user.id;
 
-                console.log('redirect:', `${Environment.FRONTEND_URL}/dashboard`, user.email);
+            console.log('redirect:', `${Environment.FRONTEND_URL}/dashboard`, user.email);
 
-                res.redirect(`${Environment.FRONTEND_URL}/dashboard`);
-            } else {
-                console.error('Missing user info from Google');
-                res.redirect(`${Environment.FRONTEND_URL}/?auth=failed&error=missing_info`);
-            }
-        } catch (error) {
-            console.error('Error during OAuth2 callback:', error);
-            if (error instanceof Error) {
-                res.redirect(`${Environment.FRONTEND_URL}/?auth=failed&error=${encodeURIComponent(error.message)}`);
-            } else {
-                res.redirect(`${Environment.FRONTEND_URL}/?auth=failed&error=unknown`);
-            }
+            res.redirect(`${Environment.FRONTEND_URL}/dashboard`);
+        } else {
+            console.error('Missing user info from Google');
+            res.redirect(`${Environment.FRONTEND_URL}/?auth=failed&error=missing_info`);
         }
 
     }
@@ -86,51 +71,46 @@ export class OAuthController {
 
         const refreshToken = req.cookies?.refreshToken;
 
-        try {
-            if (!refreshToken) {
-                return res.status(401).json({ message: 'No refresh token provided' });
-            }
-
-            const decoded = await this.oAuthservice.verifyRefreshToken(refreshToken);
-            // debugger
-            const user = await this.oAuthservice.findUserByRefreshToken(refreshToken);
-
-            console.log('USER:', user);
-            console.log('decoded:', decoded);
-
-            if (!user) {
-                throw new Error('Refresh token does not match any user');
-
-            }
-
-            if (typeof decoded === 'string') {
-                throw new Error('Invalid token structure');
-            }
-
-            // Invalidate the old token
-            await this.oAuthservice.clearRefreshToken(decoded.userId);
-
-            // Issue new Access Token
-            const accessToken = await this.oAuthservice.generateAccessToken(user);
-
-            // Generate new refresh Token
-            const newRefreshToken = await this.oAuthservice.generateRefreshToken(user);
-
-            // Save refresh token to DB
-            await this.oAuthservice.saveRefreshToken(user.id, newRefreshToken);
-
-            res.cookie('refreshToken', newRefreshToken, {
-                httpOnly: true,
-                secure: Environment.NODE_ENV === 'production',
-                sameSite: 'lax',
-                expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
-            });
-
-            res.json({ message: 'Token refreshed', accessToken });
-
-        } catch (err) {
-            return res.status(403).json({ message: 'Invalid or expired refresh token' + err });
+        if (!refreshToken) {
+            return res.status(401).json({ message: 'No refresh token provided' });
         }
+
+        const decoded = await this.oAuthservice.verifyRefreshToken(refreshToken);
+        // debugger
+        const user = await this.oAuthservice.findUserByRefreshToken(refreshToken);
+
+        console.log('USER:', user);
+        console.log('decoded:', decoded);
+
+        if (!user) {
+            throw new Error('Refresh token does not match any user');
+
+        }
+
+        if (typeof decoded === 'string') {
+            throw new Error('Invalid token structure');
+        }
+
+        // Invalidate the old token
+        await this.oAuthservice.clearRefreshToken(decoded.userId);
+
+        // Issue new Access Token
+        const accessToken = await this.oAuthservice.generateAccessToken(user);
+
+        // Generate new refresh Token
+        const newRefreshToken = await this.oAuthservice.generateRefreshToken(user);
+
+        // Save refresh token to DB
+        await this.oAuthservice.saveRefreshToken(user.id, newRefreshToken);
+
+        res.cookie('refreshToken', newRefreshToken, {
+            httpOnly: true,
+            secure: Environment.NODE_ENV === 'production',
+            sameSite: 'lax',
+            expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+        });
+
+        res.json({ message: 'Token refreshed', accessToken });
     }
 
     authUser = async (req: Request, res: Response, next: NextFunction) => {
@@ -138,21 +118,19 @@ export class OAuthController {
     }
 
     logout = async (req: Request, res: Response) => {
-        try {
-            const refreshToken = req.cookies.refreshToken;
-            const user = await this.oAuthservice.findUserByRefreshToken(refreshToken);
 
-            if (refreshToken && user) await this.oAuthservice.clearRefreshToken(user.id);
+        const refreshToken = req.cookies.refreshToken;
+        const user = await this.oAuthservice.findUserByRefreshToken(refreshToken);
 
-            res.clearCookie("refreshToken", {
-                httpOnly: true,
-                secure: Environment.NODE_ENV === "production",
-                sameSite: "strict",
-            });
-            res.sendStatus(204);
-        } catch (err) {
-            res.sendStatus(500);
-        }
+        if (refreshToken && user) await this.oAuthservice.clearRefreshToken(user.id);
+
+        res.clearCookie("refreshToken", {
+            httpOnly: true,
+            secure: Environment.NODE_ENV === "production",
+            sameSite: "strict",
+        });
+        res.sendStatus(204);
+
     };
 
     getGmailLabels = async (req: Request, res: Response, next: NextFunction) => {
