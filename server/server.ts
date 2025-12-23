4f3d1ccb0578a1708f38383f2c751c3fac11d04a
Madusanka Dissanayake <mdissanayake994@gmail.com>
2025-12-23 13:36:16 +0530

    feat(request validation) : introduced global request validation machanism

--- server/server.ts
+++ server/server.ts
@@ -3,12 +3,11 @@ import Environment from './src/config/env.config';
 import app from './src/index';
 import { logger } from './src/utils/logger';
 
-
 async function bootstrap() {
     try {
         await connetDB();
 
-        const server = app.listen(Environment.PORT, () => {
+        app.listen(Environment.PORT, () => {
             logger.info("Server started on port:" + `http://localhost:${Environment.PORT}`);
         });
 
--- server/src/routes/v1/test.ts
+++ server/src/routes/v1/test.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-
 import { TestController } from "../../controllers/test.controller";
+import { validate } from "../../middlewares/request.validate";
+import { createUserSchema } from "../../dtos/user.dto";
 
 const testController = new TestController();
 
 const router = Router();
 
-router.post('/test-route', testController.testMethod);
+router.post('/test-route', validate(createUserSchema), testController.testMethod);
+
 export default router;
